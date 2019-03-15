f56a8d84b29dc21f5cbf8edc000ed33feb9708d3
Daniel <danielhuffer@live.com>
2019-03-16 03:08:36 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ pip install --user opencv-python
 pip install --user h5py
 pip install --user pillow
 pip install --user matplotlib==2.0
+pip install --user tensorflow-gpu==1.2.0
 
 luarocks install hdf5
