de2e8a60b7024f54126a750d12c85578f0759372
Daniel <danielhuffer@live.com>
2019-03-16 05:03:22 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ pip install --user easydict
 pip install --user tqdm
 pip install --user opencv-python
 pip install --user h5py
+pip install --user pillow	
+pip install --user matplotlib==2.0	
+pip install --user tensorflow-gpu==1.2.0
 
 luarocks install hdf5
